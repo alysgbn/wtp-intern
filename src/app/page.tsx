159a41d030abf6c3ada4c0b8b7ca1c877da4511d
alysgbn <aliyah.sagaban22@gmail.com>
2025-02-11 09:29:47 +0800

    Template

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,12 @@
 import Image from "next/image";
-
+import "./globals.scss";
+import Navbar from "./components/Navbar";
 export default function Home() {
   return (
-  <div className="text-generalSans"> Walk the Planet</div>
+    <div className="text-generalSans">
+      {/* Navbar  */}
+      <Navbar />
+      Walk the Planet
+    </div>
   );
 }
